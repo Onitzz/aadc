aa5f60e2327d05a40656a7bfd5d29ca343815cd6
Onitzz <benjamin.soufflet.info@gmail.com>
2016-12-23 13:02:55 +0100

    ajout de parallax

--- js/codejs.js
+++ js/codejs.js
@@ -1,9 +1,30 @@
 
 $(document).ready(function() {
+  $.fn.parallax = function(options){
+    var $$ = $(this);
+    offset = $$.offset();
+    var defaults = {
+      'start': 0,
+      'stop': offset.top + $$.height(),
+      'coeff': 0.50
+    };
+    var opts = $.extend(defaults, options);
+    return this.each(function(){
+      $(window).bind('scroll', function() {
+        windowTop = $(window).scrollTop();
+        if((windowTop >= opts.start) && (windowTop <= opts.stop)) {
+          newCoord = windowTop * opts.coeff;
+          $$.css({
+              'background-position': '0 '+ newCoord + 'px'
+          });
+        }
+      });
+    });
+  };
 
-	$('.Collage').collagePlus({
-		'allowPartialLastRow' : true
-	});
+// call the plugin
+$('.section').parallax({ 'coeff':-0.65 });
+$('.section .inner').parallax({ 'coeff':1.15 });
 
 });
 

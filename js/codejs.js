9540a758c3d34545a101309482993e60ff7fd8dc
OnitZ <benjamin.soufflet.info@gmail.com>
2016-12-24 17:28:36 +0100

    Modification de la mise en page
    -ajout de la section "cours"
    -modification de la section "contact"
    -ajout du plugin jquery.mosaicflow pour la mosaique s'image de la
    section cours

--- js/codejs.js
+++ js/codejs.js
@@ -1,31 +1,11 @@
 
 $(document).ready(function() {
-  $.fn.parallax = function(options){
-    var $$ = $(this);
-    offset = $$.offset();
-    var defaults = {
-      'start': 0,
-      'stop': offset.top + $$.height(),
-      'coeff': 0.50
-    };
-    var opts = $.extend(defaults, options);
-    return this.each(function(){
-      $(window).bind('scroll', function() {
-        windowTop = $(window).scrollTop();
-        if((windowTop >= opts.start) && (windowTop <= opts.stop)) {
-          newCoord = windowTop * opts.coeff;
-          $$.css({
-              'background-position': '0 '+ newCoord + 'px'
-          });
-        }
-      });
-    });
-  };
+  var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 
-// call the plugin
-$('.section').parallax({ 'coeff':-0.65 });
-$('.section .inner').parallax({ 'coeff':1.15 });
+	// build scenes
+	new ScrollMagic.Scene({triggerElement: "#parallax1"})
+					.setTween("#parallax1 > div", {y: "60%", ease: Linear.easeNone})
+					.addIndicators()
+					.addTo(controller);
 
 });
-
-
